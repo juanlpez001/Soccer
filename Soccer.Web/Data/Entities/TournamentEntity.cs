35157aa0af57982c8324f7f2d350198ee2b0f853
juanlpez001 <pablinlr2011@gmail.com>
2020-09-14 17:44:13 -0500

    Controller Explanation3 > Add Helpers to upload images (Logo)

--- Soccer.Web/Data/Entities/TournamentEntity.cs
+++ Soccer.Web/Data/Entities/TournamentEntity.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Soccer.Web.Data.Entities
 {
-    public class TournamentEntity
+    public class TournamentEntity 
     {
         public int Id { get; set; }
 
---
+++ Soccer.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,16 @@
+﻿using Soccer.Web.Data.Entities;
+using Soccer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        TeamEntity ToTeamEntity(TeamViewModel model, string path, bool isNew);
+
+        TeamViewModel ToTeamViewModel(TeamEntity teamEntity);
+    }
+}
